a0c849ca6d7f225562404a3e4a165cbbd925778e
ak-fishbarc <rybarczyk.ak@gmail.com>
2024-04-13 20:13:29 +0100

    Added setup for Postgres: SQLAlchemy, Migrations and User Model to test out with PgAdmin if the code works

--- __init__.py
+++ __init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
-from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, init, upgrade, migrate
+
 from routes import login_and_registration as lar
 from routes import home_page as hp
 
 
-def create_app():
+def create_app(config):
     app = Flask(__name__)
-    app.config.from_object(Config)
+    app.config.from_object(config)
 
     app.register_blueprint(hp.create_home_page_blueprint())
     app.register_blueprint(lar.create_login_and_registration_blueprint())
@@ -14,5 +16,34 @@ def create_app():
     return app
 
 
-if __name__ == '__main__':
-    create_app().run(debug=True)
+def create_db(app):
+    sql_db = SQLAlchemy(app)
+
+    return sql_db
+
+
+def create_models(app, db):
+    with app.app_context():
+        db.create_all()
+        db.session.commit()
+
+
+def create_migrate(app, db):
+    migration = Migrate(app, db)
+    return migration
+
+
+def initialize_migration(app):
+    with app.app_context():
+        init()
+
+
+def db_migrate(app):
+    with app.app_context():
+        migrate()
+
+
+def db_migrate_upgrade(app):
+    with app.app_context():
+        upgrade()
+
---
+++ models.py
@@ -0,0 +1,24 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+def create_user_model(db):
+
+    class User(db.Model):
+        id: Mapped[int] = mapped_column(primary_key=True)
+        username: Mapped[str] = mapped_column(String(16), index=True, unique=True)
+        email: Mapped[str] = mapped_column(String(120), index=True)
+        password_hashed = mapped_column(String(256))
+
+        def set_password(self, password):
+            self.password_hashed = generate_password_hash(password)
+
+        def check_password(self, password):
+            return check_password_hash(self.password_hashed, password)
+
+        def __repr__(self):
+            return "<Username: %r>" % self.username
+
+    return User
+
