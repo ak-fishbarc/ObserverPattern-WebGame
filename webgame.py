393fcdec7017a9a9b6e02fc0c8f64b321d03678a
ak-fishbarc <rybarczyk.ak@gmail.com>
2024-04-13 20:30:25 +0100

    Small change to create user_model variable

--- webgame.py
+++ webgame.py
@@ -7,7 +7,7 @@ import models
 
 game_app = create_app(Config)
 sql_database = create_db(game_app)
-models.create_user_model(sql_database)
+user_model = models.create_user_model(sql_database)
 create_models(game_app, sql_database)
 migration = create_migrate(game_app, sql_database)
 
