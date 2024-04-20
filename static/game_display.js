4ac6b3d0fc9107da6522c3a942aa23e835a6b479
ak-fishbarc <rybarczyk.ak@gmail.com>
2024-04-20 08:27:19 +0100

    Small addition to player_profile, set up for form validation. Small addition to game_display. Mouse click will be working only when clicking on the red square. Small restructuring of the code.

--- static/game_display.js
+++ static/game_display.js
@@ -4,8 +4,7 @@ var display_context = display.getContext('2d');
 display_context.fillStyle = "red";
 display_context.fillRect(15, 25, 75, 75);
 
-
-function changeOptionsDisplay(event)
+function fetchClubs()
 {
     fetch('/produce_club/Ygplt7XxflI8gO2')
     .then(function(response)
@@ -20,9 +19,20 @@ function changeOptionsDisplay(event)
 
         js_script.setAttribute("src", "/static/show_range_value.js");
         js_script.setAttribute("type", "text/javascript");
-        document.head.appendChild(js_script)
+        document.head.appendChild(js_script);
         document.getElementById('OptionsDisplay').innerHTML = html;
     })
+}
+
+function changeOptionsDisplay(event)
+{
+    let y = event.offsetX;
+    let x = event.offsetY;
+
+    if (y > 15 && y < (15+75) && x > 25 && x < (25+75))
+    {
+        fetchClubs();
+    }
 }
 
 
