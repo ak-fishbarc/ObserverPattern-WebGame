ffc8b209b542dfec3cc09ae99d5fdbdcb81bbd1b
ak-fishbarc <rybarczyk.ak@gmail.com>
2024-04-23 10:05:31 +0100

    Added simple pagination system to display news.

--- routes/news_board.py
+++ routes/news_board.py
@@ -1,12 +1,23 @@
 from flask import Blueprint
-from flask import render_template, redirect, url_for
+from flask import render_template
 
 
 def create_news_board_blueprint(app, db, nosql_db, user_model, mail):
     news_board_bp = Blueprint('news_board', __name__, template_folder='templates')
 
-    @app.route('/news_board')
-    def news_board():
-        return render_template('news_board.html')
+    @app.route('/news_board/')
+    @app.route('/news_board/<page_number>')
+    def news_board(page_number=0):
+        news = {"name": "Somenews", "address": "Someaddress", "image": "/static/news_image.png"}
+        news_to_show = [news for i in range(0, 100)]
+        number_of_pages = int(len(news_to_show)/10)
+        if int(page_number) > number_of_pages:
+            return "No more news"
+        else:
+            paginate_news = (int(page_number) * 10)
+            limit_to = (int(paginate_news) + 10)
+            paginated_news = news_to_show[paginate_news:limit_to]
+            number_of_pointers = range(0, number_of_pages)
+        return render_template('news_board.html', news_to_show=paginated_news, number_of_pages=number_of_pointers)
 
     return news_board_bp
--- static/news_flash.js
+++ static/news_flash.js
@@ -5,14 +5,18 @@ const news_three = {name: "Deal", address: "someaddressforlink", image: "news_im
 const news_to_flash = [news_one, news_two, news_three]
 const news_space = document.getElementById("NewestNews");
 
+document.getElementById("NewsImage").src = `/static/${news_to_flash[0].image}`
 
 for (let story in news_to_flash)
 {
     let button = document.createElement("button");
+    button.style.width = "50px";
+    button.style.height = "15px";
     button.onclick = function(e){
     document.getElementById("NewsImage").src = `/static/${news_to_flash[story].image}`, false}
     news_space.appendChild(button);
 }
+
 
 
 
